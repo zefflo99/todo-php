f2791db6d3a82f3130df0294291e54ae82d065bd
kylian <kylianclerc@fedora>
2024-01-29 16:02:10 +0100

    toggle completed

--- src/index.php
+++ src/index.php
@@ -1,13 +1,12 @@
-
 <?php
 
-// load to-do in json
 $todos = [];
 if (file_exists('todos.json')) {
     $todos = json_decode(file_get_contents('todos.json'), true);
 }
 
-if (isset($_POST['ddButton'])) {
+
+if (isset($_POST['addButton'])) {
     $newTodo = $_POST['inputName'];
     if (!empty($newTodo)) {
         $todos[] = ['text' => $newTodo, 'completed' => false];
@@ -25,18 +24,20 @@ if (isset($_POST['deleteButton'])) {
 }
 
 
+if (isset($_POST['toggleCompleted'])) {
+    $indexToToggleCompleted = $_POST['toggleCompleted'];
+    if (isset($todos[$indexToToggleCompleted])) {
+        $todos[$indexToToggleCompleted]['completed'] = !$todos[$indexToToggleCompleted]['completed'];
+        saveTodos($todos);
+    }
+}
 
-
-function saveTodos() {
+// Sauvegarde des tâches dans le fichier JSON
+function saveTodos($todos) {
     file_put_contents('todos.json', json_encode($todos, JSON_PRETTY_PRINT));
     header('Location: index.php');
     exit();
 }
-{
-
-}
-
-
 ?>
 
 <!doctype html>
@@ -79,21 +80,22 @@ function saveTodos() {
     </label>
     <button type="submit" name="addButton">Submit</button>
 </form>
+
 <?php foreach ($todos as $index => $todo): ?>
-    <form action="index.php" method="post">
+    <form method="post" action="index.php">
         <div class="todo">
             <div>
-                <input type="checkbox" <?= $todo['completed'] ? 'checked' : '' ?> />
                 <span class="editable" contenteditable="true"><?= $todo['text'] ?></span>
-                <input type="hidden" name="editedTextIndex" value="<?= $index ?>" />
+                <input type="hidden" name="editedTextIndex" value="<?= $index ?>">
             </div>
             <div class="todo-controls">
                 <button type="submit" name="deleteButton" value="<?= $index ?>">Delete</button>
                 <button type="submit" name="moveUpButton" value="<?= $index ?>">Move Up</button>
                 <button type="submit" name="moveDownButton" value="<?= $index ?>">Move Down</button>
+                <button type="submit" name="toggleCompleted" value="<?= $index ?>">Toggle Completed</button>
             </div>
         </div>
     </form>
-<?php endforeach;?>
+<?php endforeach; ?>
 </body>
 </html>
